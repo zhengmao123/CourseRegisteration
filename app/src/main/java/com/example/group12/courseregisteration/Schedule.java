9438ffe0a0a9b7304e18081833d3dcd0f5806d20
Bin He <hebinbinhe@hotmail.com>
2018-06-08 14:22:28 -0300

    initial commit

--- app/src/main/java/com/example/group12/courseregisteration/Schedule.java
+++ app/src/main/java/com/example/group12/courseregisteration/Schedule.java
@@ -3,11 +3,11 @@ package com.example.group12.courseregisteration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Schedule extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_schedule);
     }
 }
