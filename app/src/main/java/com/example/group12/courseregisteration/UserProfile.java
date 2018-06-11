61427955985968604a857799fdc3aedbd8764b81
Bin He <hebinbinhe@hotmail.com>
2018-06-11 17:20:01 -0300

    initial commit

--- app/src/main/java/com/example/group12/courseregisteration/UserProfile.java
+++ app/src/main/java/com/example/group12/courseregisteration/UserProfile.java
@@ -8,6 +8,6 @@ public class UserProfile extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
+        setContentView(R.layout.activity_user_profile);
     }
 }
