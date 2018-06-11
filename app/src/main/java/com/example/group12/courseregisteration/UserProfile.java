decda3c1c40dfa0bbab9e8938a3e5d26326a5182
Bin He <hebinbinhe@hotmail.com>
2018-06-11 14:37:58 -0300

    initial commit

--- app/src/main/java/com/example/group12/courseregisteration/UserProfile.java
+++ app/src/main/java/com/example/group12/courseregisteration/UserProfile.java
@@ -3,11 +3,11 @@ package com.example.group12.courseregisteration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SelectTerm extends AppCompatActivity {
+public class UserProfile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_select_term);
+        setContentView(R.layout.activity_main_menu);
     }
 }
