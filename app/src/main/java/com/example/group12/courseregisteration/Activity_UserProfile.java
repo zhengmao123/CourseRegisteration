cf065a0359000b8e48089c37db2d1da16f3fdbec
Bin He <hebinbinhe@hotmail.com>
2018-06-12 22:30:13 -0300

    Initial commit Empty activities are created Circle CI is set by Bin He

--- app/src/main/java/com/example/group12/courseregisteration/Activity_UserProfile.java
+++ app/src/main/java/com/example/group12/courseregisteration/Activity_UserProfile.java
@@ -19,7 +19,6 @@ public class Activity_UserProfile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
     }
