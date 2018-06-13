98d05cd318d27d0876df7afc2026bdae611d8421
Bin He <hebinbinhe@hotmail.com>
2018-06-13 12:28:40 -0300

    Second commit [Refactor] Sign in functionality is added Sign in UI is created by Bin He and Chasteen Redondo

--- app/src/main/java/com/example/group12/courseregisteration/Activity_SignIn.java
+++ app/src/main/java/com/example/group12/courseregisteration/Activity_SignIn.java
@@ -2,7 +2,6 @@ package com.example.group12.courseregisteration;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.text.TextUtils;
@@ -10,18 +9,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.TextView;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import android.graphics.Color;
 
 public class Activity_SignIn extends AppCompatActivity {
 
     Button buttonSignIn;
     private EditText editTextEmail;
     private EditText editTextPassword;
+    private TextView message;
+
     private FirebaseAuth mAuth;
-    private ProgressDialog progressText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,18 +34,16 @@ public class Activity_SignIn extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-        if (mAuth.getCurrentUser() != null)
+        if (mAuth.getCurrentUser() == null)
         {
             startActivity(new Intent(getApplicationContext(),Activity_UserProfile.class));
         }
 
 
-        progressText = new ProgressDialog(this);
-
         buttonSignIn = (Button)findViewById(R.id.buttonSignIn);
         editTextEmail = (EditText)findViewById(R.id.editTextEmail);
         editTextPassword = (EditText)findViewById(R.id.editTextPassword);
-
+        message = (TextView) findViewById(R.id.Message);
 
 
         buttonSignIn.setOnClickListener(new View.OnClickListener() {
@@ -65,35 +65,31 @@ public class Activity_SignIn extends AppCompatActivity {
         String password = editTextPassword.getText().toString().trim();
 
         if (TextUtils.isEmpty(email)){
-
             Toast.makeText(this, "Please enter email",Toast.LENGTH_SHORT).show();
             return;
-
         }
 
         if (TextUtils.isEmpty(password)){
             Toast.makeText(this, "Please enter password",Toast.LENGTH_SHORT).show();
             return;
-
         }
-
-        progressText.setMessage("Signing in...");
-        progressText.show();
-
 
 
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        progressText.dismiss();
+
                         if (task.isSuccessful()) {
+                            //finish();
+                            message.setText("Verification success!");
+                            message.setTextColor(Color.GREEN);
                             finish();
                             startActivity(new Intent(getApplicationContext(), Activity_UserProfile.class));
                         }
                         else{
-                            Toast.makeText(Activity_SignIn.this, "Could not sign in", Toast.LENGTH_SHORT).show();
-                            progressText.hide();
+                            message.setText("Invaild email or password!");
+                            message.setTextColor(Color.RED);
                         }
 
                     }
