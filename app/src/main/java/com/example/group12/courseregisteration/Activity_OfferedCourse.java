54e43e68183082c80668988a0135dacbb5452fa6
Owner <j94679@gmail.com>
2018-06-13 18:54:16 -0300

    Offered Courses functionality added by P. Chartier and Xiaoze Xia

--- app/src/main/java/com/example/group12/courseregisteration/Activity_OfferedCourse.java
+++ app/src/main/java/com/example/group12/courseregisteration/Activity_OfferedCourse.java
@@ -1,5 +1,7 @@
 package com.example.group12.courseregisteration;
 
+//test test
+
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
