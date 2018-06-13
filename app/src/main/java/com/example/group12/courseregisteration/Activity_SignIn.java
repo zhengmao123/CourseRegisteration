1e7a192b63b74906616c6457d2672a32f3591942
Bin He <hebinbinhe@hotmail.com>
2018-06-12 22:24:51 -0300

    Initial commit Empty activities are created

--- app/src/main/java/com/example/group12/courseregisteration/Activity_SignIn.java
+++ app/src/main/java/com/example/group12/courseregisteration/Activity_SignIn.java
@@ -15,11 +15,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class Activity_Schedule extends AppCompatActivity {
+public class Activity_SignIn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_schedule);
+        setContentView(R.layout.activity__sign_in);
     }
 }
--- app/src/main/java/com/example/group12/courseregisteration/Activity_UserProfile.java
+++ app/src/main/java/com/example/group12/courseregisteration/Activity_UserProfile.java
@@ -2,6 +2,18 @@ package com.example.group12.courseregisteration;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Activity_UserProfile extends AppCompatActivity {
 
