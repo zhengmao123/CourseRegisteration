1772ec13dd07cac98ad221f8f98913ba24bd2eaa
Bin He <hebinbinhe@hotmail.com>
2018-06-12 22:42:40 -0300

    Second commit Sign in functionality is added Sign in UI is created by Bin He and Chasteen Redondo

--- app/src/main/java/com/example/group12/courseregisteration/Activity_SignIn.java
+++ app/src/main/java/com/example/group12/courseregisteration/Activity_SignIn.java
@@ -17,9 +17,89 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class Activity_SignIn extends AppCompatActivity {
 
+    private Button buttonSignIn;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private FirebaseAuth mAuth;
+    private ProgressDialog progressText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__sign_in);
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+        if (mAuth.getCurrentUser() != null)
+        {
+            startActivity(new Intent(getApplicationContext(),Activity_UserProfile.class));
+        }
+
+
+        progressText = new ProgressDialog(this);
+
+        buttonSignIn = (Button)findViewById(R.id.buttonSignIn);
+        editTextEmail = (EditText)findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText)findViewById(R.id.editTextPassword);
+
+
+
+        buttonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                userSignIn();
+
+            }
+        });
+
+
     }
+
+
+    public void userSignIn(){
+
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)){
+
+            Toast.makeText(this, "Please enter email",Toast.LENGTH_SHORT).show();
+            return;
+
+        }
+
+        if (TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Please enter password",Toast.LENGTH_SHORT).show();
+            return;
+
+        }
+
+        progressText.setMessage("Signing in...");
+        progressText.show();
+
+
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressText.dismiss();
+                        if (task.isSuccessful()) {
+                            finish();
+                            startActivity(new Intent(getApplicationContext(), Activity_UserProfile.class));
+                        }
+                        else{
+                            Toast.makeText(Activity_SignIn.this, "Could not sign in", Toast.LENGTH_SHORT).show();
+                            progressText.hide();
+                        }
+
+                    }
+                });
+
+    }
+
+
 }
