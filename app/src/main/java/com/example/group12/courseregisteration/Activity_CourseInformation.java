8f24175c0bd68514e3c03f7e0e9fda7ee9e4fba5
Bin He <hebinbinhe@hotmail.com>
2018-06-13 13:35:32 -0300

    Second commit [Refactor] add import by Bin He

--- app/src/main/java/com/example/group12/courseregisteration/Activity_CourseInformation.java
+++ app/src/main/java/com/example/group12/courseregisteration/Activity_CourseInformation.java
@@ -2,7 +2,6 @@ package com.example.group12.courseregisteration;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.text.TextUtils;
@@ -10,10 +9,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.TextView;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import android.graphics.Color;
 
 public class Activity_CourseInformation extends AppCompatActivity {
 
