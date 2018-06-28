bc4bfdc2870862ae8fbd23c262c9e2ca080affe7
Owner <j94679@gmail.com>
2018-06-28 07:50:46 -0300

    Registration
    by Peter & Mao

--- app/src/main/java/com/example/group12/courseregisteration/Activity_OfferedCourses.java
+++ app/src/main/java/com/example/group12/courseregisteration/Activity_OfferedCourses.java
@@ -1,5 +1,8 @@
 package com.example.group12.courseregisteration;
 
+//to-do
+//add javadoc and expresso
+
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
